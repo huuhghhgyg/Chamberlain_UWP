ad4d82abfaf0a7ddb7b7e963568b0bb0f1ad57bd
huuhghhgyg <huuhghhgyg@outlook.com>
2022-04-10 13:59:53 +0800

    导航栏和标题栏使用Mica

--- Chamberlain_UWP/MainPage.xaml.cs
+++ Chamberlain_UWP/MainPage.xaml.cs
@@ -6,8 +6,11 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.ApplicationModel.Background;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +28,62 @@ namespace Chamberlain_UWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            SetTitleBar();
+
+            SettingsConfig.InitialLoad();
+
+            InitializeData(initializeNavigate); // 使用回调（Action），先data后navigate
+        }
+
+        private void SetTitleBar()
+        {
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+
+            titleBar.ButtonBackgroundColor = Colors.Transparent;
+            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+
+            // 隐藏默认TitleBar。Hide default title bar.
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+            UpdateTitleBarLayout(coreTitleBar);
+
+            // 设置为可拖动区域 Set XAML element as a draggable region.
+            Window.Current.SetTitleBar(AppTitleBar);
+
+            // 分辨率变化的处理
+            // Register a handler for when the size of the overlaid caption control changes.
+            // For example, when the app moves to a screen with a different DPI.
+            coreTitleBar.LayoutMetricsChanged += CoreTitleBar_LayoutMetricsChanged;
+
+            // 当窗口激活状态改变时，注册一个handler
+            //Register a handler for when the window changes focus
+            Window.Current.Activated += Current_Activated;
+        }
+        private void UpdateTitleBarLayout(CoreApplicationViewTitleBar coreTitleBar)
+        {
+            // 更新TitleBar的大小，适应窗口大小
+            AppTitleBar.Height = coreTitleBar.Height;
+
+            // 保证自定义TitleBar不会覆盖标题控件
+            Thickness currMargin = AppTitleBar.Margin;
+            AppTitleBar.Margin = new Thickness(currMargin.Left, currMargin.Top, coreTitleBar.SystemOverlayRightInset, currMargin.Bottom);
+        }
+        private void CoreTitleBar_LayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            UpdateTitleBarLayout(sender);
+        }
+        private void Current_Activated(object sender, Windows.UI.Core.WindowActivatedEventArgs e) // 根据是否处于活动状态更新TitleBar
+        {
+            SolidColorBrush defaultForegroundBrush = (SolidColorBrush)Application.Current.Resources["TextFillColorPrimaryBrush"];
+            SolidColorBrush inactiveForegroundBrush = (SolidColorBrush)Application.Current.Resources["TextFillColorDisabledBrush"];
+
+            if (e.WindowActivationState == Windows.UI.Core.CoreWindowActivationState.Deactivated) AppTitle.Foreground = inactiveForegroundBrush;
+            else AppTitle.Foreground = defaultForegroundBrush;
+        }
 
         private async void InitializeData(Action callback)
         {
@@ -38,15 +97,6 @@ namespace Chamberlain_UWP
         {
             contentFrame.Navigate(typeof(TaskPage));
             navControl.SelectedItem = navControl.MenuItems[0];
-        }
-
-        public MainPage()
-        {
-            this.InitializeComponent();
-
-            SettingsConfig.InitialLoad();
-
-            InitializeData(initializeNavigate); // 使用回调（Action），先data后navigate
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -79,6 +129,37 @@ namespace Chamberlain_UWP
 
         private const string taskName = "ChamberlainBackgroundUpdater";
         private const string taskEntryPoint = "BackgroundUpdater.DateUpdater";
+
+        // 根据NavigationView的显示模式更新TitleBar布局（宽/窄）
+        // Update the TitleBar content layout depending on NavigationView DisplayMode
+        private void NavigationViewControl_DisplayModeChanged(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewDisplayModeChangedEventArgs args)
+        {
+            const int topIndent = 16;
+            const int expandedIndent = 48;
+            int minimalIndent = 104;
+
+            // If the back button is not visible, reduce the TitleBar content indent.
+            if (navControl.IsBackButtonVisible.Equals(Microsoft.UI.Xaml.Controls.NavigationViewBackButtonVisible.Collapsed))
+            {
+                minimalIndent = 48;
+            }
+
+            Thickness currMargin = AppTitleBar.Margin;
+
+            // Set the TitleBar margin dependent on NavigationView display mode
+            if (sender.PaneDisplayMode == Microsoft.UI.Xaml.Controls.NavigationViewPaneDisplayMode.Top)
+            {
+                AppTitleBar.Margin = new Thickness(topIndent, currMargin.Top, currMargin.Right, currMargin.Bottom);
+            }
+            else if (sender.DisplayMode == Microsoft.UI.Xaml.Controls.NavigationViewDisplayMode.Minimal)
+            {
+                AppTitleBar.Margin = new Thickness(minimalIndent, currMargin.Top, currMargin.Right, currMargin.Bottom);
+            }
+            else
+            {
+                AppTitleBar.Margin = new Thickness(expandedIndent, currMargin.Top, currMargin.Right, currMargin.Bottom);
+            }
+        }
 
         private void navControl_ItemInvoked(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewItemInvokedEventArgs args)
         {
