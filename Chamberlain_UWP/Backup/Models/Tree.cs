09cb6f2062141001c1f03068b25fe470af73f780
huuhghhgyg <huuhghhgyg@outlook.com>
2022-07-08 16:47:13 +0800

    构建核心

---
+++ Chamberlain_UWP/Backup/Models/Tree.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Chamberlain_UWP.Backup.Models
+{
+    internal class FolderNode //文件夹节点
+    {
+        public string Name { get; set; } //文件夹名称
+        public int foldernum { get => Folders.Count; } //文件夹下的文件夹数量
+        public StorageFolder Folder { get; set; } //可供操作的文件夹对象
+        public List<FolderNode> Folders { get; set; } //此文件夹下的文件夹列表，便于往下访问
+        public List<FileNode> FileNodes { get; set; } //此文件夹下的文件节点列表
+
+        public FolderNode(string name, StorageFolder folder, List<FolderNode> folders, List<FileNode> fileNodes)
+        {
+            Name = name;
+            Folder = folder;
+            Folders = folders;
+            FileNodes = fileNodes;
+        }
+    }
+
+    internal class FileNode //文件节点
+    {
+        public StorageFile File { get; set; } //可供操作的文件对象
+        public string Hash { get; set; } //文件哈希值
+
+        public FileNode(StorageFile file, string hash)
+        {
+            File = file;
+            Hash = hash;
+        }
+    }
+}
---
+++ Chamberlain_UWP/Backup/Models/TreeManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Security.Cryptography;
+using Windows.Security.Cryptography.Core;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace Chamberlain_UWP.Backup.Models
+{
+    internal static class TreeManager
+    {
+        public static async Task<string> GetMD5HashAsync(StorageFile file) //可用
+        {
+            string algorithmName = HashAlgorithmNames.Md5;
+            IBuffer buffer = await FileIO.ReadBufferAsync(file);
+
+            HashAlgorithmProvider algorithmProvider = HashAlgorithmProvider.OpenAlgorithm(algorithmName);
+
+            IBuffer buffHash = algorithmProvider.HashData(buffer);
+
+            if (buffHash.Length != algorithmProvider.HashLength)
+            {
+                throw new Exception("创建hash时出现问题");
+            }
+
+            string hex = CryptographicBuffer.EncodeToHexString(buffHash);
+
+            return hex;
+        }
+
+        static async Task<FolderNode> GetFolder(StorageFolder folder)
+        {
+            // Application now has read/write access to all contents in the picked folder
+            // (including other sub-folder contents)
+            //Windows.Storage.AccessCache.StorageApplicationPermissions.
+            //FutureAccessList.AddOrReplace("PickedFolderToken", folder);
+            string folderName = folder.Name;
+
+            IReadOnlyList<StorageFolder> folder_list = await folder.GetFoldersAsync();
+
+            IReadOnlyList<StorageFile> file_list = await folder.GetFilesAsync();
+            
+            return null;
+        }
+
+    }
+}
