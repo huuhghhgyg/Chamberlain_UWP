040f40dcbc78a474e236f28d2df9ef52f65ad5d7
huuhghhgyg <huuhghhgyg@outlook.com>
2022-09-04 19:58:17 +0800

    ViewModelBase新增方法

--- Chamberlain_UWP/Backup/ViewModel/ViewModelBase.cs
+++ Chamberlain_UWP/Backup/ViewModel/ViewModelBase.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,6 +15,16 @@ namespace Chamberlain_UWP.Backup
         {
             PropertyChangedEventHandler handler = this. PropertyChanged;
             if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public bool Set<T>(ref T target, T value, [CallerMemberName] string propertyName = null)
+        {
+            //如果value无变化
+            if(EqualityComparer<T>.Default.Equals(target, value)) return false;
+
+            //目标值与value不同
+            target = value;
+            OnPropertyChanged(propertyName);
+            return true;
         }
     }
 }
